aade7c2707b4acff9b8bd51a2927d2af15b9267e
Carlos <carlos1990627@gmail.com>
2026-07-18 10:40:17 -0500

    Coordenadas del jugador

--- js/mokepon.js
+++ js/mokepon.js
@@ -416,6 +416,9 @@ function pintarMapa() {
         map.height
     )
     playerPetObject.pintarMokepon()
+
+    enviarPosicion(playerPetObject.x, playerPetObject.y)
+
     hipodogeEnemy.pintarMokepon()
     capipepoEnemy.pintarMokepon()
     ratigueyaEnemy.pintarMokepon()
@@ -431,6 +434,19 @@ function pintarMapa() {
         revisarColision(pydosEnemy)
     }
 
+}
+
+function enviarPosicion(x, y) {
+    fetch(`http://localhost:8080/mokepon/${jugadorId}/posicion`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            x,
+            y
+        })
+    })
 }
 
 function moverDerecha() {
